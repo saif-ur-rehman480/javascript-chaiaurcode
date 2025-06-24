b9ccf69ea634d740e9c5d2365fb64de13b88b416
saif-ur-rehman480 <saify6188@gmail.com>
2025-06-24 18:11:22 +0000

    datatypes variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345;
+let accountEmail = "saif@gmail.com"
+var accountPassword = "fuck@1911"
+accountCity = "khanewal"
+let accountState;
+
+
+// accountId = 4444;
+accountEmail = "saify@gmail.com"
+accountPassword = "saify@1911"
+accountCity = "lahore"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+// console.log(accountId)
+
+/* prefer to use let instead of var
+because of block and functional scope */
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+// "use strict"; // treat all js code as newer version
+// primitive datatypes
+// number => 2 to power 53
+// bigint => it is used in stock trading like big number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+ 
