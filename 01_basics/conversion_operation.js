be657fe1ebc2b4e166e57b8d89da9b5b4250083b
saif-ur-rehman480 <saify6188@gmail.com>
2025-06-25 17:49:03 +0000

    datatype_conversion confusion

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,32 @@
+let score = "49abc"
+// console.log(typeof score)
+// console.log(typeof(score));
+let valueInBoolean = Boolean(score)
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(valueInBoolean)
+// console.log(typeof valueInBoolean);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let valueInTrue = Boolean(isLoggedIn)
+console.log(valueInTrue);
+
+// 1 => true; 0=> false
+// "" => false
+// "saif" => true
+
+let number = 33
+let valueInString = String(number)
+console.log(valueInString);
+console.log(typeof(valueInString));
+
+
+
+
+
+
