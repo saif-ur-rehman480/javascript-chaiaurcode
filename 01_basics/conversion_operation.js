89ce6f56eb1d23d1c5743d75c406db27d72c249b
saif-ur-rehman480 <saify6188@gmail.com>
2025-06-26 02:25:38 +0000

    operations

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -28,13 +28,81 @@ let valueInString = String(number)
 
 let random = undefined
 let randomValueInNumber = Number(random)
-console.log(randomValueInNumber);
-console.log(typeof randomValueInNumber);
+// console.log(randomValueInNumber);
+// console.log(typeof randomValueInNumber);
 
 let random2 = null
 let randomValueInNumber2 = Number(random2)
-console.log(randomValueInNumber2);
-console.log(typeof randomValueInNumber2);
+// console.log(randomValueInNumber2);
+// console.log(typeof randomValueInNumber2);
+
+// ************************ Operations ***********************
+let value = 45;
+let negValue = -value;
+// console.log(negValue);
+
+// Arithmetic operators
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+// String concatenation
+let str1 = "hello"
+let str2 = " saif"
+let str3 = str1 + str2
+// console.log(str3);
+
+// Implicit type conversion
+
+console.log("1" + 2); /* 12  */
+console.log(1 + "2"); /* 12  */
+console.log("1" + 2 + 2); /* 122 */
+console.log(1 + 2 + "2"); /* 32 */
+
+// these are question to practice
+// console.log("2" + 1 + 1) 211
+// console.log(1 + 1 + "2") 22
+// console.log(true + "false") truefalse
+// console.log("10" - true) 9
+// console.log("5" - "2") 3
+// console.log("5" + "2") 52
+// console.log(3 + false + "7") 37
+// console.log("6" / "2") 3
+// console.log("hello" + true + 5) hellotrue5
+// console.log("10" * 2) 20
+
+// operator precedence
+console.log((2+2)*3/6**2);
+
+console.log(+true);
+console.log(+"");
+
+// postfix and prefix
+let x = 10
+let y = x++
+console.log(x);
+console.log(y)
+
+
+let a = 30
+let b = ++a
+console.log(a);
+console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
